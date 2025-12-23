e7f4f5e0dcae3b4debf01658d23d9f34b5325a3f
harshayukotiya18 <harshayukotiya18@gmail.com>
2025-12-23 12:47:03 +0530

    Add auth pages, layouts, guards and API service

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import DefaultRoute from './Guards/DefaultRoute';
+import UserLayout from './Layout/UserLayout';
+import AdminLayout from './Layout/AdminLayout';
+import UserDashboard from './Pages/UserLayout/Dashboard';
+import AdminDashboard from './Pages/AdminLayout/Dashboard';
+import MyBookings from './pages/UserLayout/MyBookings';
+import UserEvent from './pages/UserLayout/Event';
+import AdminEvent from './pages/AdminLayout/Event';
+import Profile from './pages/UserLayout/Profile';
+import Booking from './pages/AdminLayout/Booking';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />
+    },
+     {
+      path: "/register",
+      element: <Register />
+    },
+    {
+      path:"/",
+      element: <DefaultRoute />
+    },
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // User Routes
+    {
+      path:"user",
+      element: <UserLayout />,
+      children: [
+        {path: "dashboard", element: <UserDashboard />},
+        {path: "my-bookings", element: <MyBookings />},
+        {path: "event", element: <UserEvent />},
+        {path: "profile", element: <Profile/> }
+      ]
+    },
+
+    // admin routes
+    {
+      path:"admin",
+      element: <AdminLayout />,
+      children: [
+        {path: "dashboard", element: <AdminDashboard/>},
+        {path: "bookings", element: <Booking />},
+        {path: "event", element: <AdminEvent />},
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
